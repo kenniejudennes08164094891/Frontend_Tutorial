99293042ff70bb72ed385dd5386c1b45f6cdb140
Kehinde Omosehin <kehindejude1995@gmail.com>
2025-07-14 11:02:03 +0100

    feature: Creating React Apps with Tailwind and Flowbite

---
+++ src/pages/dasboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashboard(){
+    return(
+        <>
+        <div>
+            <h1 className="text-blue-700">Hello Word</h1>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard;
---
+++ src/pages/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login(){
+    return(
+        <>
+        <div>
+            <h1 className="text-blue-700">Welcome to the Login Page</h1>
+        </div>
+        </>
+    )
+}
+
+export default Login;
