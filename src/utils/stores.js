3f317c5148dc77241de98f0caad4b348fe354ef0
Olorunda Victory <chidiolorunda@gmail.com>
2025-07-23 21:52:58 +0100

    File Upload Assignment

--- src/utils/stores.js
+++ src/utils/stores.js
@@ -2,4 +2,12 @@ export const referenceVariableData = {
   localStorage: "localStorage",
   sessionStorage: "sessionStorage",
   navigation: "navigation",
-}
+};
+
+export const HttpConstants = {
+  POST: "POST",
+  GET: "GET",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
