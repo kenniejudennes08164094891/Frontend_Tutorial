97c2db9bb5367b70fb369309d2dcb693cde25f3c
Kehinde Omosehin <kehindejude1995@gmail.com>
2025-08-04 10:40:26 +0100

    feature: My first Angular Project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      scrollbar: {
+        enabled: true,
+      },
+
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
